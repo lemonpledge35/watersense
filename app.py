ced0c71359dc089ab0947cde10ab3e5a804fa1e3
Matthew Loyd <mstarrloyd@gmail.com>
2024-12-16 16:48:42 -0500

    debugging

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/", methods = ['GET','POST'])
 def home():
-    value = read(11)
+    value = read_pin(11)
     if value == 1:
         text = "false"
     elif value == 0:
